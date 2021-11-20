eb7997ef9727d650406d1902d8b605de448f2819
deboracaires <deboracaires@pop-os.localdomain>
2021-11-20 17:19:34 -0300

    havia um erro na pagina de cadastro, o sqal fire estava com conteudo errado para quando o usuario digitasse um email invalido

--- src/components/loginPage.js
+++ src/components/loginPage.js
@@ -1,18 +1,33 @@
-import { Title } from "../styles/initialPageStyle";
-import { ButtonLogin, Conteiner, Input, SecondButton} from "../styles/loginAndSignUpStyle";
+import { useState } from 'react';
+import Swal from 'sweetalert2';
+import { Title } from '../styles/initialPageStyle';
+import { ButtonLogin, Conteiner, Input, SecondButton} from '../styles/loginAndSignUpStyle';
 
 export default function LoginPage() {
     
-    function doLogin(){
-
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    function doLogin(e){
+        e.preventDefault();
+        if (email.substring(email.length - 4) !== '.com') {
+            Swal.fire('Digite um nome com no mínimo 5 caracteres');
+        } 
     }
     
     return (
         <Conteiner>
             <Title>Bem vindo ao GratiBox</Title>
             <form onSubmit={doLogin}>
-                <Input placeholder = 'Email'></Input>
-                <Input placeholder = 'Senha'></Input>
+                <Input type = 'text'
+                    value = {email}
+                    placeholder = 'Nome'
+                    onChange={(e) => setEmail(e.target.value)}>
+                </Input>
+                <Input type = 'text'
+                    value = {password}
+                    placeholder = 'Nome'
+                    onChange={(e) => setPassword(e.target.value)}>
+                </Input>
                 <ButtonLogin type = 'submit'>Entrar</ButtonLogin>
             </form>
             <SecondButton>Ainda não sou grato</SecondButton>
