f8195bb03edfc34c42e78a1b693f7217c812f61e
deboracaires <87550053+deboracaires@users.noreply.github.com>
2021-11-22 18:11:51 -0300

    colocando o link do heroky

--- src/services/api.services.js
+++ src/services/api.services.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const URL = 'http://localhost:4000';
+const URL = 'https://gratibox-back-deboracaires.herokuapp.com';
 
 function registerUser (body) {
     return axios.post(`${URL}/sign-up`, body);
